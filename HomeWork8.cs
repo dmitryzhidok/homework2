fb9adc5e5abbca9ec38d8f88cf4906ef25761fdf
testeddima <testeddima@gmail.com>
2023-06-27 22:00:19 +0300

    HomeWork8

---
+++ HomeWork8.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+var dog = new Dog();
+dog.Name = "sharik";
+dog.SetName(dog.Name);
+dog.Eat();
+abstract class Anymal
+{
+    public string Name { get; set; }
+    public void SetName(string name)
+    { this.Name = name; }
+    public string GetName()
+    {
+        return Name;
+    }
+    public abstract void Eat();
+    
+}
+class Dog : Anymal
+{
+    public override void Eat()
+    {
+        Console.WriteLine($"{Name} eat meat ");
+    }
+}
